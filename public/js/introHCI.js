d63a24a810c5ad6be598515c1252402e14635684
vasuki <sunder.vasuki@gmail.com>
2018-02-14 22:43:41 -0800

    ajax and facebook lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,10 +9,14 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+
+
 	$('.project a').click(addProjectDetails);
 
 	$('#colorBtn').click(randomizeColors);
 }
+
+
 
 /*
  * Make an AJAX call to retrieve project details and add it in
@@ -27,4 +31,14 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, callBackFn); 
+
+}
+
+function callBackFn(result){
+console.log(result);
+var insertedText = '<p>' + result.title + ' </p>' + '<p>' + result.date +
+ '</p>' + '<img src="' + result.image +  '" class="detailsImage">' + result.summary;
+$('#project' + result.id + ' .details').html(insertedText);
+
 }
